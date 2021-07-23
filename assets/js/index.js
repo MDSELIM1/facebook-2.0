7bc575c9efedb1d8af34d4e9bd324170ab8c2f0c
MDSELIM1 <selimakodo58@gmail.com>
2021-07-23 15:56:39 +0600

    final-add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,37 @@
+// Setting Menu Javascript 
 
 var settingsMenu = document.querySelector('.profile-settings');
 
 var darkBtn = document.getElementById("dark-mode");
 
 function settingsMenuToggle() {
-    settingsMenu.classList.toggle("settings-menu-height")
+    settingsMenu.classList.toggle("settings-menu-height");
 }
+
 
 darkBtn.onclick = function () {
     this.classList.toggle('dark-btn-on');
-    document.body.classList.toggle('dark-color')
+    document.body.classList.toggle('dark-color');
+
+    // Dark theme / Light theme 
 
     if (localStorage.getItem("theme") == "light") {
-        localStorage.setItem("theme", "dark")
-    }
-    else {
-        localStorage.setItem("theme", "light")
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
     }
 
 }
+
+
+// Dark Mode / Light Mode Stay On When Browser Refresh 
 
 if (localStorage.getItem("theme") == "light") {
     darkBtn.classList.remove('dark-btn-on');
     document.body.classList.remove('dark-color');
-}
-
-else if (localStorage.getItem("theme") == "dark") {
+} else if (localStorage.getItem("theme") == "dark") {
     darkBtn.classList.add('dark-btn-on');
     document.body.classList.add('dark-color');
-}
-
-else {
-    localStorage.setItem("theme", "light")
+} else {
+    localStorage.setItem("theme", "light");
 }
